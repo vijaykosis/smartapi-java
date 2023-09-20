db4428d2550b8ce491343c639f247dff061cebbc
acc-sameer <sameer.shaikh@acc.ltd>
2023-09-20 09:21:33 +0530

    git comments

--- src/main/java/com/angelbroking/smartapi/sample/Test.java
+++ src/main/java/com/angelbroking/smartapi/sample/Test.java
@@ -3,21 +3,13 @@ package com.angelbroking.smartapi.sample;
 import com.angelbroking.smartapi.SmartConnect;
 import com.angelbroking.smartapi.http.exceptions.SmartAPIException;
 import com.angelbroking.smartapi.models.User;
-import com.warrenstrange.googleauth.GoogleAuthenticator;
 
 public class Test {
 
 	public static void main(String[] args) throws SmartAPIException {
 		try {
-			SmartConnect smartConnect = new SmartConnect("zkWvUuLx"); // PROVIDE YOUR API KEY HERE
-			String clientId = "D541276";
-			GoogleAuthenticator gAuth = new GoogleAuthenticator();
-			String totp_key = "L6FMTTCWRVSK2PW6AF7A2YMO6Q";
-			String tOTP = String.valueOf(gAuth.getTotpPassword(totp_key));
-			User user = smartConnect.generateSession("D541276", "1512", tOTP);
-			smartConnect.setAccessToken(user.getAccessToken());
-			smartConnect.setUserId(user.getUserId());
-//			SmartConnect smartConnect = new SmartConnect("<api_key>"); // PROVIDE YOUR API KEY HERE
+
+			SmartConnect smartConnect = new SmartConnect("<api_key>"); // PROVIDE YOUR API KEY HERE
 
 			/*
 			 * OPTIONAL - ACCESS_TOKEN AND REFRESH TOKEN SmartConnect smartConnect = new
@@ -45,70 +37,70 @@ public class Test {
 
 			Examples examples = new Examples();
 			/* System.out.println("getProfile"); */
-//			examples.getProfile(smartConnect);
+			examples.getProfile(smartConnect);
 
 			/* System.out.println("placeOrder"); */
-//			examples.placeOrder(smartConnect);
-//
-//			/* System.out.println("modifyOrder"); */
-//			examples.modifyOrder(smartConnect);
-//
-//			/* System.out.println("cancelOrder"); */
-//			examples.cancelOrder(smartConnect);
-//
-//			/* System.out.println("getOrder"); */
-//			examples.getOrder(smartConnect);
-//
-//			/* System.out.println("getLTP"); */
-//			examples.getLTP(smartConnect);
-//
-//			/* System.out.println("getTrades"); */
-//			examples.getTrades(smartConnect);
-//
-//			/* System.out.println("getRMS"); */
-//			examples.getRMS(smartConnect);
-//
-//			/* System.out.println("getHolding"); */
-//			examples.getHolding(smartConnect);
-//
-//			/* System.out.println("getPosition"); */
-//			examples.getPosition(smartConnect);
-//
-//			/* System.out.println("convertPosition"); */
-//			examples.convertPosition(smartConnect);
-//
-//			/* System.out.println("createRule"); */
-//			examples.createRule(smartConnect);
-//
-//			/* System.out.println("ModifyRule"); */
-//			examples.modifyRule(smartConnect);
-//
-//			/* System.out.println("cancelRule"); */
-//			examples.cancelRule(smartConnect);
-//
-//			/* System.out.println("Rule Details"); */
-//			examples.ruleDetails(smartConnect);
-//
-//			/* System.out.println("Rule List"); */
-//			examples.ruleList(smartConnect);
-//
-//			/* System.out.println("Historic candle Data"); */
-//			examples.getCandleData(smartConnect);
-//
-//			/* System.out.println("Market Data"); */
+			examples.placeOrder(smartConnect);
+
+			/* System.out.println("modifyOrder"); */
+			examples.modifyOrder(smartConnect);
+
+			/* System.out.println("cancelOrder"); */
+			examples.cancelOrder(smartConnect);
+
+			/* System.out.println("getOrder"); */
+			examples.getOrder(smartConnect);
+
+			/* System.out.println("getLTP"); */
+			examples.getLTP(smartConnect);
+
+			/* System.out.println("getTrades"); */
+			examples.getTrades(smartConnect);
+
+			/* System.out.println("getRMS"); */
+			examples.getRMS(smartConnect);
+
+			/* System.out.println("getHolding"); */
+			examples.getHolding(smartConnect);
+
+			/* System.out.println("getPosition"); */
+			examples.getPosition(smartConnect);
+
+			/* System.out.println("convertPosition"); */
+			examples.convertPosition(smartConnect);
+
+			/* System.out.println("createRule"); */
+			examples.createRule(smartConnect);
+
+			/* System.out.println("ModifyRule"); */
+			examples.modifyRule(smartConnect);
+
+			/* System.out.println("cancelRule"); */
+			examples.cancelRule(smartConnect);
+
+			/* System.out.println("Rule Details"); */
+			examples.ruleDetails(smartConnect);
+
+			/* System.out.println("Rule List"); */
+			examples.ruleList(smartConnect);
+
+			/* System.out.println("Historic candle Data"); */
+			examples.getCandleData(smartConnect);
+
+			/* System.out.println("Market Data"); */
 			examples.getMarketData(smartConnect);
-//
-//			/* System.out.println("logout"); */
-//			examples.logout(smartConnect);
-//
-//			/* SmartAPITicker */
-//			String clientId = "<clientId>";
-//			User user = smartConnect.generateSession("<clientId>", "<password>", "<totp>");
-//			String feedToken = user.getFeedToken();
-//			String strWatchListScript = "nse_cm|2885&nse_cm|1594&nse_cm|11536&mcx_fo|221658";
-//			String task = "mw";
-//
-//			examples.tickerUsage(clientId, feedToken, strWatchListScript, task);
+
+			/* System.out.println("logout"); */
+			examples.logout(smartConnect);
+
+			/* SmartAPITicker */
+			String clientId = "<clientId>";
+			User user = smartConnect.generateSession("<clientId>", "<password>", "<totp>");
+			String feedToken = user.getFeedToken();
+			String strWatchListScript = "nse_cm|2885&nse_cm|1594&nse_cm|11536&mcx_fo|221658";
+			String task = "mw";
+
+			examples.tickerUsage(clientId, feedToken, strWatchListScript, task);
 
 			/*
 			 * String jwtToken = user.getAccessToken(); String apiKey = "smartapi_key";
